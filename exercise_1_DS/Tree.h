8caaa39da25bfe03df5ff703f2978fe3dcde8cda
avishay farkash <62483176+avishf7@users.noreply.github.com>
2020-10-18 12:55:50 +0300

    עידכון שרץ - עם תיעוד

--- exercise_1_DS/Tree.h
+++ exercise_1_DS/Tree.h
@@ -3,29 +3,30 @@
 #include <iostream>
 #include <list>
 using namespace std;
-class Tree
+class Tree//Multi-boy tree-Represents a forum discussion
 {
 private:
 
-	class Node
+	class Node//node in tree
 	{
 	public:
-		string content;
-		list<Node*>responses;
-		~Node()
+		string content;//Content of the discussion
+		list<Node*>responses;//the responses
+		~Node()//detor-Deletes the entire subtree of this node
 		{
 			for (auto it = responses.begin(); it != responses.end(); it++)
 				delete* it;
 		}
 	};
 
-	Node* root;
+	Node* root;//the root of the tree
 
+	//Recursive function-Receives a string and returns a pointer to the node that contains it
 	Node* Search(string discussion, Node* current)
 	{
-		if (current->content == discussion)
+		if (current->content == discussion)//If it is in the current node-return pointer to it
 			return current;
-		for (auto it = current->responses.begin(); it != current->responses.end(); it++)
+		for (auto it = current->responses.begin(); it != current->responses.end(); it++)//search if it's in one of the boys
 		{
 			Node* find = Search(discussion, *it);
 			if (find != NULL)
@@ -34,6 +35,7 @@ private:
 		return NULL;
 	}
 
+	//Recursive function-Gets a string and returns a pointer to the father of the node that contains it
 	Node* seasrchFather(string discussion, Node* current)
 	{
 		for (auto it = current->responses.begin(); it != current->responses.end(); it++)
@@ -48,23 +50,24 @@ private:
 		return NULL;
 	}
 
-
+	//Recursive function-Prints the tree hierarchically
 	void print(Node* current, int space)
 	{
-		for (int i = 0; i < space * 3; i++)
+		for (int i = 0; i < space * 3; i++)//Print space as needed
 			cout << " ";
 
-		cout << current->content << endl;
+		cout << current->content << endl;//Print the contents of the node
 
-		for (auto it = current->responses.begin(); it != current->responses.end(); it++)
+		for (auto it = current->responses.begin(); it != current->responses.end(); it++)//Print his sons
 			print(*it, space + 1);
 	}
 
+	//Recursive function-Gets a string and prints the route from the root to the node
 	void printMaslul(string discussion, Node* current, int space)
 	{
-		cout << current->content;
+		cout << current->content;//print the current node
 
-		for (auto it = current->responses.begin(); it != current->responses.end(); it++)
+		for (auto it = current->responses.begin(); it != current->responses.end(); it++)//Find out where the string is and continue printing according to the route
 		{
 			Node* find = Search(discussion, *it);
 			if (find == NULL)
@@ -74,10 +77,12 @@ private:
 		}
 	}
 public:
-	Tree() { root = NULL; }
 
-	string getRootString() const { return root->content; }  //getter
+	Tree() { root = NULL; }//ctor
 
+	string getRootString() const { return root->content; }  //getter-Returns the root contents
+
+	//Puts a new string at the root and delete the old tree
 	void setroot(string discussion)
 	{
 		delete root;
@@ -85,22 +90,24 @@ public:
 		root->content = discussion;
 	}
 
-	~Tree()
+	~Tree()//dtor
 	{
 		delete root;
 		root = NULL;
 	}
 
+	//Receives a string and returns a pointer to the node that contains it
 	Node* Search(string discussion)
 	{
-		return Search(discussion, root);
+		return Search(discussion, root);//Calls a recursive function
 	}
 
+	//Gets a string and a response string on it, and adds the response to the tree
 	bool add(string father, string son)
 	{
-		Node* fatherP = Search(father);
+		Node* fatherP = Search(father);//find the node of the father
 
-		if (fatherP)
+		if (fatherP)//If there is such a string in the tree
 		{
 			Node* newrespones = new Node;
 			newrespones->content = son;
@@ -110,9 +117,10 @@ public:
 		return fatherP;
 	}
 
+	//Gets a string and delete all of its sub-tree from the tree
 	bool remove(string discussion)
 	{
-		if (root->content == discussion)
+		if (root->content == discussion)//If you need to delete the root
 		{
 			delete root;
 			return (root = NULL);
@@ -120,35 +128,36 @@ public:
 		}
 		else
 		{
-			Node* toRemove = Search(discussion);
+			Node* toRemove = Search(discussion);//Find the node to delete
 			if (toRemove)
 			{
-				Node* toRemoveFather = seasrchFather(discussion, root);
+				Node* toRemoveFather = seasrchFather(discussion, root);//find its father
 
-				toRemoveFather->responses.remove(toRemove);
+				toRemoveFather->responses.remove(toRemove);//delete from the tree
 			}
 
 			return toRemove;
 		}
 	}
 
+	//Prints the tree hierarchically
 	void print()
 	{
 		if (root)
-			print(root, 0);
+			print(root, 0);//Calls a recursive function
 	}
+
+	//Gets a stringand prints the route from the root to the node
 	void printMaslul(string discussion)
 	{
 		if (Search(discussion))
-			printMaslul(discussion, root, 0);
+			printMaslul(discussion, root, 0);//Calls a recursive function
 	}
+
+	//Gets a string and prints its subtree
 	void printSubTree(string discussion)
 	{
 		if (Node* rootToPrint = Search(discussion))
-			print(rootToPrint, 0);
+			print(rootToPrint, 0);//Calls a recursive function
 	}
 };
-
-
-
-
