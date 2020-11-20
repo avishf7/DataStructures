1145c0dbd8d28c9235659932a33f317a4652b239
avishay farkash <62483176+avishf7@users.noreply.github.com>
2020-11-20 14:20:07 +0200

    תיעוד

--- exercise_2_DS/Item.h
+++ exercise_2_DS/Item.h
@@ -1,7 +1,14 @@
+//Avishay Farkash 205918790
+//Shai Axelrod 205544307
+
 #ifndef ITEM_H
 #define ITEM_H
 
+// Represents the status of one item in the table:
 enum state { empty, full, deleted };
+
+// Each enter in the hash table is a item object that include the key
+// Which determines the index and the data 
 template <class T, class K>
 class Item
 {
@@ -9,7 +16,11 @@ public:
 	T data;
 	K key;
 	state flag;
-	Item() {}
+
+	//CTOR
+	Item() {} 
+
+	//CTOR - Initializes all fields
 	Item(T d, K k, state f) { data = d; key = k; flag = f; }
 };
 
--- exercise_2_DS/MyHashTable.h
+++ exercise_2_DS/MyHashTable.h
@@ -1,3 +1,6 @@
+//Avishay Farkash 205918790
+//Shai Axelrod 205544307
+
 #ifndef MYHASHTABLE_H
 #define MYHASHTABLE_H
 
@@ -13,12 +16,14 @@ protected:
 	int size;
 
 
-	virtual int h1(K key) = 0;
-	virtual int h2(K key) = 0;
-	int hash(K key, int i);
+	virtual int h1(K key) = 0; //Determining a starting point(abstract)
+	virtual int h2(K key) = 0; //Skip function((abstract)
+	int hash(K key, int i); 
 public:
+	
 	MyHashTable(int size);
 
+	//DTOR
 	~MyHashTable()
 	{
 		for (int i = 0; i < size; i++)
@@ -33,12 +38,15 @@ public:
 	int search(K key);
 };
 
+// hash function
 template<class T, class K>
 int MyHashTable<T, K>::hash(K key, int i)
 {
 	return (h1(key) + i * h2(key)) % size;
 }
 
+// CTOR - Finds a prime number greater than the size 
+// and initializes the table to be the size of the same number
 template<class T, class K>
 MyHashTable<T, K>::MyHashTable(int size)
 {
@@ -48,14 +56,17 @@ MyHashTable<T, K>::MyHashTable(int size)
 	for (j = size; !isFound; isFound = !isDiv, isDiv = false, j++)
 		if (!(isDiv = !(j % 2 != 0)))
 			for (int i = 3; !isDiv && i <= sqrt(size); i += 2)
-				isDiv = (j % i == 0);
+				isDiv = (j % i == 0); // if the number is divided that means that not a prime number
 
 	table = new Item<T, K> * [--j];
 	this->size = j;
+	//Initializes the entire table
 	for (int i = 0; i < j; i++)
 		table[i] = new Item<T, K>();
 }
 
+// Print all the data of all the keys 
+// in the table that not empty or deleted
 template<class T, class K>
 void MyHashTable<T, K>::print()
 {
@@ -68,6 +79,7 @@ void MyHashTable<T, K>::print()
 
 }
 
+// Updates the data on a specific item
 template<class T, class K>
 void MyHashTable<T, K>::update(T data, K key)
 {
@@ -78,6 +90,7 @@ void MyHashTable<T, K>::update(T data, K key)
 		std::cout << "ERROR\n";
 }
 
+// Deletes the data in a specific item (changes its state to deleted)
 template<class T, class K>
 void MyHashTable<T, K>::remove(K key)
 {
@@ -87,6 +100,7 @@ void MyHashTable<T, K>::remove(K key)
 	
 }
 
+// Adds a new item to the table
 template<class T, class K>
 void MyHashTable<T, K>::add(T data, K key)
 {
@@ -103,6 +117,8 @@ void MyHashTable<T, K>::add(T data, K key)
 	std::cout << "ERROR\n";
 }
 
+// Returns the index of an item in the table 
+// and if it is not found returns 1
 template<class T, class K>
 int MyHashTable<T, K>::search(K key)
 {
