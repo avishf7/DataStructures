fb9c12850f9f0edf954fcd24df7a658d28778fd4
avishay farkash <62483176+avishf7@users.noreply.github.com>
2020-12-13 20:03:58 +0200

    פונקציית הכנסה ומחיקה לעץ תחילויות

---
+++ exercise_3_DS/Trie.cpp
@@ -0,0 +1,50 @@
+#include "Trie.h"
+
+void Trie::Insert(string str)
+{
+	insert(str, root);
+
+}
+
+bool Trie::Delete(string str)
+{
+	TrieNode* last;
+
+	if (!(last = Search(str)))
+		return false;
+
+	last->isEndWord = false;
+
+	for (auto i = last; i != NULL, i->count == 0;)
+		{
+			i->father->children[str[str.length() - 1]] = NULL;
+			i->father->count--;
+			last = i;
+			i = i->father;
+			delete last;
+			str = str.substr(0, str.length() - 1);
+		}
+
+	return true;
+}
+
+void Trie::insert(string str, TrieNode* node)
+{
+	if (str.length() == 0)
+		return;
+
+	char ch = str[0];
+
+
+	if (node->children[ch - 97] == NULL)
+	{
+		node->children[ch - 97] = new TrieNode(node);
+		node->count++;
+	}
+
+	if (str.length() == 1)
+		node->children[ch - 97]->isEndWord = true;
+
+
+	insert(str.substr(1, str.length() - 1), node->children[ch - 97]);
+}
---
+++ exercise_3_DS/Trie.h
@@ -0,0 +1,40 @@
+//Avishay Farkash 205918790
+//Shai Axelrod 205544307
+#ifndef TRIE_H
+#define TRIE_H
+#include <string>
+using namespace std;
+
+class Trie
+{
+protected:
+	class TrieNode
+	{
+	public:
+		bool isEndWord = false;
+		TrieNode* children[26] = { NULL };
+		TrieNode* father;
+		int count = 0;
+
+		TrieNode(TrieNode* father = NULL) :father(father) {}
+	};
+
+	TrieNode* root = new TrieNode();
+
+public:
+
+
+
+	void Insert(string str);
+	bool Delete(string str);
+	TrieNode* Search(string str);
+	void PrintAllWordsFromPrefix(TrieNode*);
+private:
+	bool search(string str, TrieNode* node);
+	void insert(string str, TrieNode* node);
+	void search(string str, TrieNode* node);
+
+};
+
+#endif
+
