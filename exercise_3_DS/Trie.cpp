72da8013b4d9f9a77e4231c44c1004e1bd433ddb
avishay farkash <62483176+avishf7@users.noreply.github.com>
2020-12-14 14:24:18 +0200

    תיעוד - אבישי

--- exercise_3_DS/Trie.cpp
+++ exercise_3_DS/Trie.cpp
@@ -5,11 +5,12 @@ using namespace std;
 void Trie::Insert(string str)
 {
 	insert(str, root);
-
 }
 
 void Trie::insert(string str, TrieNode* node)
 {
+	//Stops when reaching the end of inserting the string 
+	//or inserting an empty string
 	if (str.length() == 0)
 		return;
 
@@ -18,13 +19,15 @@ void Trie::insert(string str, TrieNode* node)
 
 	if (node->children[ch - 97] == NULL)
 	{
+		//Inserts a character into the tree 
+		//in case there is no character in the track
 		node->children[ch - 97] = new TrieNode(node);
 		node->count++;
 	}
 
+	//Changes the flag after inserting the last letter:
 	if (str.length() == 1)
 		node->children[ch - 97]->isEndWord = true;
-
 
 	insert(str.substr(1, str.length() - 1), node->children[ch - 97]);
 }
@@ -36,8 +39,12 @@ bool Trie::Delete(string str)
 	if (!last)
 		return false;
 
-	last->isEndWord = false;
+	//Change the flag for a case where there are other words in the tree
+	//that contain the deleted word
+	last->isEndWord = false; 
 
+	//Deletes all characters that have been prompted to delete
+	//in case there are no more words using characters:
 	for (TrieNode* i = last; i->father != NULL && i->count == 0;)
 	{
 		i->father->children[str[str.length() - 1] - 97] = NULL;
@@ -55,11 +62,11 @@ bool Trie::Delete(string str)
 
 Trie::TrieNode* Trie::findPrefix(string str, TrieNode* node)
 {
-	if (str.length() == 0)
+	if (str.length() == 0)//We reached a node representing the end of the string
 		return node;
 
 	char ch = str[0];
-	if (node->children[ch - 97] == NULL)
+	if (node->children[ch - 97] == NULL)//One letter is missing
 		return	NULL;
 	return findPrefix(str.substr(1, str.length() - 1), node->children[ch - 97]);
 }
--- exercise_3_DS/Trie.h
+++ exercise_3_DS/Trie.h
@@ -26,7 +26,16 @@ protected:
 
 public:
 
+	//Receives a string 
+	//and sends to a recursive function, that will 
+	//insert the string into the word management tree 
+	//each letter individually.
 	void Insert(string str);
+	//Gets a string 
+	//and deletes or cancels (changes the value of IsEndOfWord to false) 
+	//The string obtained from the  word management tree
+	// and returns false In case the string does not exist
+	//otherwise return true.
 	bool Delete(string str);
 	
 	bool Search(string str);//shai
@@ -35,8 +44,17 @@ public:
 private:
 
 	TrieNode* find(string str, TrieNode* node);//shai
+	//recursive function, that will
+	//insert the string into the word management tree 
+	//each letter individually.
 	void insert(string str, TrieNode* node);
+
 	void PrintAllWordsFromPrefix(string str, TrieNode* node);//shai
+
+	//A recursive function that gets a string(not necessarily a whole word)
+	//and searches for it in the word management tree
+	//and returns the node that represents the end of the prefix,
+	//in case the string does not exist returns NULL.
 	TrieNode* findPrefix(string str, TrieNode* node);
 	
 };
